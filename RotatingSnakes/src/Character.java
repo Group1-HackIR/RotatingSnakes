097cc5d7eb53bcf85e72902524dc8ef02d8270db
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2018-12-01 12:51:54 -0500

    SecondCommit

---
+++ RotatingSnakes/src/Character.java
@@ -0,0 +1,25 @@
+import java.awt.image.BufferedImage;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Character {
+	int angle = 0;
+	int speed = 7;
+	int Charx = 0;
+	int CHary = 0;
+	BufferedImage i;
+	public Character(BufferedImage ik) {
+		i = ik;
+		Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			public void run() {
+				if(angle>=360) {
+					angle = angle-360;
+				}
+				if(angle<=0) {
+					angle = 360+angle;
+				}
+			}
+		}, 0,20);
+	}
+}
--- RotatingSnakes/src/Hackathon.java
+++ RotatingSnakes/src/Hackathon.java
@@ -8,40 +8,38 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Hackathon extends JPanel {
+public class Hackathon extends JPanel{
 	static BufferedImage character1;
 	static BufferedImage character2;
 	static {
 		try {
-			character1 = ImageIO.read(Hackathon.class.getResource("Char1.jpg"));
-			character2 = ImageIO.read(Hackathon.class.getResource("Char2.jpg"));
-		} catch (IOException e) {
+			character1 = ImageIO.read(Hackathon.class.getResource("Char1,jpg"));
+			character2 = ImageIO.read(Hackathon.class.getResource("Char2,jpg"));
+		}catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 	public void Action() {
 		Timer t = new Timer();
 		t.schedule(new TimerTask() {
 			public void run() {
 				repaint();
 			}
-		}, 0, 20);
+		}, 0,20);
 	}
-
+	
 	public void paint(Graphics g) {
-		g.drawString(x + "," + y, 50, 50);
+		g.drawString(x+","+y, 50, 50);
 	}
-
+	
 	static int x = 0;
 	static int y = 0;
-
 	public static void main(String[] args) {
 		JFrame f = new JFrame("rotating snakes");
 		Hackathon ha = new Hackathon();
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(730, 700);
+		f.setSize(730,700);
 		f.add(ha);
 		ha.Action();
 	}
