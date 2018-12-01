efab7845b389ad1062173f660e8862f8e655452a
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2018-12-01 16:06:36 -0500

    Update Rect.java

--- RotatingSnakes/src/Hackathon/Rect.java
+++ RotatingSnakes/src/Hackathon/Rect.java
@@ -3,13 +3,18 @@ package Hackathon;
 public class Rect {
 	int width; 
 	int height;
+	
+	// Array of points. Bottom-left, Bottom-right, top-left, top-right
+	Point points[] = new Point[4];
+	
 	public Rect(int w, int h) {
 		this.width = w;
 		this.height = h;
 		
 	}
 	
-	public boolean collides(){
+	public boolean collides(Rect other){
+		
 		return false;
 	}
 	
@@ -20,4 +25,9 @@ public class Rect {
 	public int getWidth() {
 		return width;
 	}
+	
+	double cosineLawRev() {
+		return 0.0d;
+	}
+	
 }
